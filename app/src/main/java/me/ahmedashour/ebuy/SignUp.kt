53ade3359a9f8b6dc952ce86f2f899a7bfeda336
Hansel Tita <hanseltita@gmail.com>
2020-10-09 23:29:18 +0700

    Sign Up activity ready

--- app/src/main/java/me/ahmedashour/ebuy/SignUp.kt
+++ app/src/main/java/me/ahmedashour/ebuy/SignUp.kt
@@ -1,11 +1,18 @@
 package me.ahmedashour.ebuy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        signIn_btn.setOnClickListener {
+
+            startActivity(Intent(this, SignIn::class.java))
+        }
     }
 }
