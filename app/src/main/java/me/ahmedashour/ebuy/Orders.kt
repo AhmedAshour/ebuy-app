044eb3545f45bb635f090a0fbe6007c0ad159033
Hansel Tita <hanseltita@gmail.com>
2020-10-09 21:48:53 +0700

    Added Activities

---
+++ app/src/main/java/me/ahmedashour/ebuy/Orders.kt
@@ -0,0 +1,11 @@
+package me.ahmedashour.ebuy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Orders : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_orders)
+    }
+}
